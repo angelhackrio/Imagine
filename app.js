6d635228fe6de1831fd21e2a902ecbf722c3e2be
Beatriz Macedo <beamb@br.ibm.com>
2016-06-26 12:00:52 -0300

    Removido packages nao usadas + app com upload de file

--- app.js
+++ app.js
@@ -27,41 +27,49 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("Server starting on " + appEnv.url);
 });
 
-var bodyParser = require('body-parser');
-
-// Configure Express
-app.use(bodyParser.urlencoded({extended: true, limit: '1mb'}));
-app.use(bodyParser.json({limit: '1mb'}));
-
-var extend = require('util')._extend;
-
-// VCAP Services for Bluemix
-var vcapServices = require('vcap_services');
-
-// Speech to Text Configuration for local dev
-var config = extend({
-  version: 'v1',
-  url: 'https://stream.watsonplatform.net/speech-to-text/api',
-  username: 'b5a5efb9-e2dd-484a-a2c2-9f716afc6383',
-  password: 'h8dxuIj6umXk'
-}, vcapServices.getCredentials('speech_to_text'));
-
-// Rendering Website
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/chat.html');
+// Get Sound File from Front-End
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+app.post('/uploadSound', multipartMiddleware, function(req, resp) {
+  console.log(req.body, req.files);
+  // don't forget to delete all req.files when done
 });
 
 // Instatiating Watson APIs
 var watson = require('watson-developer-cloud');
+var fs = require('fs');
 
-var authService = watson.authorization(config);
-
-// Speech to Text Token Authentication
-app.post('/api/token', function(req, res, next) {
-  authService.getToken({url: config.url}, function(err, token) {
-    if (err)
-      next(err);
-    else
-      res.send(token);
-  });
+var speech_to_text = watson.speech_to_text({
+  username: 'efe606e8-a364-4d75-a525-d104abb62919',
+  password: '3l4O5Pm0ioOa',
+  version: 'v1',
 });
+
+var params = {
+  content_type: 'audio/wav',
+  continuous: true,
+  interim_results: true
+};
+
+// // Create the stream.
+// var recognizeStream = speech_to_text.createRecognizeStream(params);
+//
+// // Pipe in the audio.
+// fs.createReadStream('audio-file.wav').pipe(recognizeStream);
+//
+// // Pipe out the transcription to a file.
+// recognizeStream.pipe(fs.createWriteStream('transcription.txt'));
+//
+// // Get strings instead of buffers from 'data' events.
+// recognizeStream.setEncoding('utf8');
+//
+// // Listen for events.
+// recognizeStream.on('data', function(event) { onEvent('Data:', event); });
+// recognizeStream.on('results', function(event) { onEvent('Results:', event); });
+// recognizeStream.on('error', function(event) { onEvent('Error:', event); });
+// recognizeStream.on('close-connection', function(event) { onEvent('Close:', event); });
+//
+// // Displays events on the console.
+// function onEvent(name, event) {
+//     console.log(name, JSON.stringify(event, null, 2));
+// };
